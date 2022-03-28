8d0783e5341131debdbcf9f1405d6719e1cd4017
Marc Moten <marcmotenii@gmail.com>
2022-03-28 18:22:35 -0500

    added logo and made max width Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../images/GarlandLogo.png'
 
 function Navbar() {
     return(
         <div>
             <nav>
-                <div className='logo'>Garland</div>
+                <img src={logo} className='logo'alt="Garland Logo"></img>
                 <ul className='links'>
-                    <li><a>Home</a></li>
-                    <li><a>Surf and Swim</a></li>
-                    <li><a>Bradfield Pool</a></li>
-                    <li><a>Holdford Pool</a></li>
+                    <li><a href="https://www.garlandtx.gov/154/Aquatics">Home</a></li>
+                    <li><a href="https://www.garlandtx.gov/154/Aquatics">Surf and Swim</a></li>
+                    <li><a href="https://www.garlandtx.gov/154/Aquatics">Bradfield Pool</a></li>
+                    <li><a href="https://www.garlandtx.gov/154/Aquatics">Holdford Pool</a></li> 
                 </ul>
             </nav>
         </div>
